6214f9023efd15b890930aff4c72c0dab0cffcb2
ZzAlexDev <zzalexdev@gmail.com>
2023-02-21 17:43:42 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# name = input('Enter your name: ')
+# name = "enter Your name: "
+
+# age = input('Enter your age: ')
+# city = input('Enter your city: ')
+# print(name, age, city)
+# print('\n')
+# print(dir(name))
+# print('\n')
+# print(name, age, city)
+# print(name)
+# print('\n')
+# print(name.removesuffix(3))
+# print('\n')
+
+# def my_name(name):
+# def my_name(name):
+#    print(name)
+#    my_name('Sanya')
+# my_name('Sanya')
+
+# This is comment
+
+# This
+# is
+# comment
+
+
+print(print(10+5))
+
+
